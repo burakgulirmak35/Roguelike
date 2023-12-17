0ce0fc00d579de849cffe00b1b7ccfe6626f0146
Burak <90563983+burakgulirmak35@users.noreply.github.com>
2023-12-17 22:32:31 +0300

    ++

---
+++ Assets/Dev/Scripts/Enviroment/City.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> ProbSets = new List<GameObject>();
+
+    private int currentIndex = 0;
+    public void OnEnable()
+    {
+        ProbSets[currentIndex].SetActive(false);
+        currentIndex = Random.Range(0, ProbSets.Count);
+        ProbSets[currentIndex].SetActive(true);
+    }
+}
